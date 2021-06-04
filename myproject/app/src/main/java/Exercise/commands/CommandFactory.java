3796d569fc26bbb274ed266792fb5befb966ceb8
FlorianSymmank <floriansymmank@gmx.de>
2021-06-04 16:21:17 +0200

    updated CommandFactory to use all new available Commands

--- myproject/app/src/main/java/Exercise/commands/CommandFactory.java
+++ myproject/app/src/main/java/Exercise/commands/CommandFactory.java
@@ -1,9 +1,11 @@
 package Exercise.commands;
 
+import Exercise.commands.find.SearchCommand;
+import Exercise.commands.list.*;
+import Exercise.commands.sort.ChooseSortCommand;
 import Exercise.console.IConsole;
 import Exercise.data.Student;
 import Exercise.lists.IListable;
-import Exercise.sort.SelectionSort;
 
 import java.util.LinkedList;
 
@@ -18,11 +20,23 @@ public class CommandFactory {
 
     public LinkedList<ICommand> returnCommands() {
         LinkedList<ICommand> commands = new LinkedList<ICommand>();
+
         commands.add(new ExitCommand(cnsl));
+
         commands.add(new AddCommand(cnsl, list));
-        commands.add(new SortCommand(cnsl, list, new SelectionSort<Student>()));
+        commands.add(new AddAtCommand(cnsl, list));
+        commands.add(new AddFirstCommand(cnsl, list));
+        commands.add(new AddLastCommand(cnsl, list));
+        commands.add(new GetCommand(cnsl, list));
+        commands.add(new PrintAllCommand(cnsl, list));
+        commands.add(new SizeCommand(cnsl, list));
+        commands.add(new RemoveAtCommand(cnsl, list));
+        commands.add(new RemoveAllCommand(cnsl, list));
+
+        commands.add(new SearchCommand(cnsl, list));
+
         commands.add(new ChooseSortCommand(cnsl, list));
-        //TODO: Add more commands
+
         return commands;
     }
 }
